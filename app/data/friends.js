60a12cd7092e61f7390d02f36ba7468f34002173
Stephanie Hughes <stephaniegracehughes@gmail.com>
2019-08-26 21:06:40 -0700

    updated friends variable

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,28 +1,28 @@
 var friends = [
     {
-        name: 'Kramer',
-        photoLink: 'https://vignette.wikia.nocookie.net/seinfeld/images/3/3c/Kramer.jpg/revision/latest?cb=20110401134023',
+        name: 'Friend1',
+        photoLink: 'TEST',
         answers: [4,4,4,4,4,4,4,4,4,4] 
     },
     {
-        name: 'Morty',
-        photoLink: 'https://vignette.wikia.nocookie.net/seinfeld/images/5/56/Dadseinfeld.jpg/revision/latest?cb=20120204122559',
+        name: 'Friend2',
+        photoLink: 'TEST',
         answers: [2,2,2,2,2,2,2,2,2,5]
     },
     {
-        name: 'Elaine',
-        photoLink: 'https://vignette.wikia.nocookie.net/seinfeld/images/5/57/Elaine_benes019.jpg/revision/latest?cb=20070321074208',
+        name: 'Friend3',
+        photoLink: 'TEST',
         answers: [5,5,5,5,5,5,5,5,5,5]
     },
     {
-        name: 'George',
-        photoLink: 'https://vignette.wikia.nocookie.net/seinfeld/images/7/76/George-costanza.jpg/revision/latest?cb=20110406222711',
+        name: 'Friend4',
+        photoLink: 'TEST',
         answers: [1,2,3,4,5,1,2,3,4,5]
     }
     ,
     {
-        name: 'Newman',
-        photoLink: 'https://vignette.wikia.nocookie.net/seinfeld/images/7/7f/Untitled.jpg/revision/latest?cb=20120128125849',
+        name: 'Friend5',
+        photoLink: 'TEST',
         answers: [5,4,3,2,1,1,1,1,1,1]
     }
 ];
